0e00afd7a7759840163b3b50ab7db84071a2b551
vima1013 <viktor.mazepa@NetCracker.com>
2023-12-21 11:22:44 +0200

    Add example of gitleaks pre-commit hook

---
+++ gitleaks-precommit.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+CURRENT_DIR=$(pwd)
+
+# Move to scripts folder
+cd scripts
+
+# Run validation script
+go run pre-commit.go
+
+# Get the result of script execution
+exit_code=$?
+
+# Check script's execution result
+if [ $exit_code -ne 0 ]; then
+  echo "Commit failed according to gitleaks validation"
+  exit 1
+fi
+
+exit 0
